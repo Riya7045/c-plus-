bc35820a3d060921e0b43e38cd19eb9d1775d991
Riya7045 <132160161+Riya7045@users.noreply.github.com>
2024-05-25 00:41:00 +0530

    Time: 10 ms (45.41%) | Memory: 8.2 MB (48.12%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        long long int revNum=0;
+        int dup=x;
+        while(x>0){
+            int temp=x%10;
+            revNum=(revNum*10)+temp;
+            x=x/10;
+        }
+        if(dup==revNum){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+};
