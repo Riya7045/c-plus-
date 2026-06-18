6e0dba9f514de8fe87eda11d01a7ba00ca2feb24
Riya <132160161+Riya7045@users.noreply.github.com>
2026-06-18 15:29:16 +0530

    Time: 863 ms (13.92%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select product_id, year as first_year, quantity, price 
-from sales where (product_id, year) in (
-    select product_id, min(year)
-    from sales
-    group by product_id
-)
+-- select product_id, year as first_year, quantity, price 
+-- from sales where (product_id, year) in (
+--     select product_id, min(year)
+--     from sales
+--     group by product_id
+-- )
+with cte as (select *, dense_rank() over (partition by product_id order by year) as rnk from Sales)
+
+
+select product_id, year as first_year, quantity, price from cte where rnk = 1;
