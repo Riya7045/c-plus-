c8759f1d67a5f5a825fc0a90e39eac720d95268c
Riya <132160161+Riya7045@users.noreply.github.com>
2026-07-27 01:14:49 +0530

    Time: 30 ms (33.42%) | Memory: 16.3 MB (80.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    int r;
-    int c;
-    bool valid(int i,int j){
-        return i>=0 && i<r && j>=0 && j<c;
+   
+    int m;
+    int n;
+    void dfs(vector<vector<char>>& grid,int i, int j){
+        
+         if(i<0||i>=m||j<0||j>=n||grid[i][j]=='0'||grid[i][j] != '1'){
+            return;
+         }
+         if(grid[i][j] == -1) {
+            return;
+        }
+        
+        grid[i][j] = '$'; //mark visited
+
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
+        
     int numIslands(vector<vector<char>>& grid) {
-        r = grid.size();
-        c = grid[0].size();
-        int row[4] = {-1, 1, 0, 0};
-        int col[4] = {0, 0, -1, 1};
-        queue<pair<int,int>>q;
-        int count = 0;
-        //check the char '1' in our grid
-        for(int i =0;i<r;i++){
-            for(int j=0; j<c; j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    // convert all 1 to 0
-                    q.push(make_pair(i,j));
-                    grid[i][j]='0';
-                    while(!q.empty()){
-                        int new_i = q.front().first;
-                        int new_j = q.front().second;
-                        q.pop();
-                        //8 direcctions me check krna hai
-                        for(int k =0; k<4; k++){
-                            if(valid(new_i+row[k],new_j+col[k])&&grid[new_i+row[k]][new_j+col[k]]=='1'){
-                                grid[new_i+row[k]][new_j+col[k]]='0';
-                                q.push(make_pair(new_i+row[k],new_j+col[k]));
-                            }
-                        }
-                    }
+        m    = grid.size();
+        n    = grid[0].size();
+        int island = 0;
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                if(grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    island++;
                 }
+                
             }
         }
-        return count;
+        
+        return island;
     }
 };
