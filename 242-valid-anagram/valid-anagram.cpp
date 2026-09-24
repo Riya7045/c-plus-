170e08c2fde077cdf9b642e9f9c1545c8b569bcc
Riya <132160161+Riya7045@users.noreply.github.com>
2026-09-24 22:21:19 +0530

    Time: 6 ms (21.72%) | Memory: 9.8 MB (25.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        unordered_map<char,int>counter;
+        for(int ch:s){
+            counter[ch]=counter[ch]+1;
+        }
+        for(int ch:t){
+            if(counter.find(ch)==counter.end()||counter[ch]==0){
+                return false;
+            }
+            counter[ch]=counter[ch]-1;
+        }
+        return true;
     }
 };
